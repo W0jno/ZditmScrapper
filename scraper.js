90b0c5e3aaec595bd0d67700eeefcce15e34f93b
w0jno <filip.wojno03@o2.pl>
2024-01-08 23:26:07 +0100

    Correct for of departure times

--- scraper.js
+++ scraper.js
@@ -1,51 +1,71 @@
 const puppeteer = require("puppeteer");
 
 (async () => {
-  // Otwórz przeglądarkę
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: false,
     userDataDir: "./tmp",
   });
 
-  // Otwórz nową stronę
   const page = await browser.newPage();
 
-  // Przejdź do konkretnej strony
   await page.goto(
     "https://www.zditm.szczecin.pl/pl/pasazer/rozklady-jazdy/tabliczka/46/768/p/87-dunska"
   );
 
-  const table = await page.$$(
-    ".accordion-item > div >>> table > tbody > tr > td > a > .visually-hidden"
-  );
-  /*  let hours = [];
-  let minutes = [];
+  let total = [];
+  const hours = await page.evaluate(() => {
+    const accordionItems = document.querySelectorAll(".accordion-item");
 
-  for (const field of table) {
-    const hour = await page.evaluate((el) => {
-      const text = el.querySelector(
-        "div>div>div>div>div>table>thead>tr"
-      ).textContent;
-      return text.trim().replace(/\s+/g, " ");
-    }, field);
-    const minute = await page.evaluate((el) => {
-      const text = el.querySelector(
-        "div>div>div>div>div>table>tbody>tr"
-      ).textContent;
-      return text.trim().replace(/\s+/g, " ");
-    }, field);
-    hours.push(hour);
-    minutes.push(minute);
+    return Array.from(accordionItems).map((accordionItem) => {
+      const hoursElements = accordionItem.querySelectorAll(
+        "div >div> div> div> table > tbody > tr > td > a > .visually-hidden"
+      );
+      return Array.from(hoursElements).map((hour) =>
+        hour.textContent.replace(/\s+/g, " ")
+      );
+    });
+  });
+
+  const days = await page.$$eval(
+    ".accordion-header > button > div > div",
+    (daysElements) => daysElements.map((day) => day.textContent.trim())
+  );
+
+  for (let i = 0; i < days.length; i++) {
+    total.push(days[i]);
+    total.push(hours[i]);
   }
-  console.log(minutes); */
-  let departureTimes = [];
-  for (const field of table) {
-    const departureTime = await page.evaluate((el) => {
-      return el.textContent.replace(/\s+/g, " ");
-    }, field);
-    departureTimes.push(departureTime);
+
+  const isWeekday = () => {
+    const now = new Date();
+    if (now.getDay() < 6 && now.getDay() > 0) return true;
+  };
+  const isSaturday = () => {
+    const now = new Date();
+    if (now.getDay() === 6) return true;
+  };
+  const isSunday = () => {
+    const now = new Date();
+    if (now.getDay() === 0) return true;
+  };
+
+  const nearestDeparture = (departureTimes) => {
+    // TO DO
+  };
+
+  if (isWeekday) {
+    const nearest = nearestDeparture(total[1]);
+    console.log(`Najbliższy odjazd w dni robocze: ${nearest}`);
+  } else if (isSaturday) {
+    const nearest = nearestDeparture(total[3]);
+    console.log(`Najbliższy odjazd w sobotę: ${nearest}`);
+  } else if (isSunday) {
+    const nearest = nearestDeparture(total[5]);
+    console.log(`Najbliższy odjazd w niedzielę: ${nearest}`);
   }
-  console.log(departureTimes);
+
+  //console.log(getTimeInMinutes(total[1][0]));
+
   await browser.close();
 })();
